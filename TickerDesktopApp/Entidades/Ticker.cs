5c17dcd286353942e431d3b6257995d6f4b3a927
Luca Provenzano <Luca_Provenzano@intranet.itresources>
2018-09-05 17:36:39 -0300

    first commit

---
+++ TickerDesktopApp/Entidades/Ticker.cs
@@ -0,0 +1,92 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Ticker
+    {
+        string nombre;
+        string codigo;
+        string cotizacion;
+        string porcentaje;
+
+        private string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+
+        }
+
+        private string Codigo
+        {
+            get
+            {
+                return this.codigo.ToUpper();
+            }
+        }
+
+        private string Porcentaje
+        {
+            get
+            {
+                return this.porcentaje;
+            }
+        }
+
+        public Ticker(string codigo)
+        {
+            try
+            {
+                var html = @"https://finance.yahoo.com/quote/" + codigo;
+                HtmlWeb web = new HtmlWeb();
+                var htmlDoc = web.Load(html);
+
+                HtmlNode nombreActivo = htmlDoc.DocumentNode.SelectSingleNode("//h1[@class='D(ib) Fz(18px)']");
+                HtmlNode cotizActivo = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)']");
+                HtmlNode porcentajeActivo = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($dataGreen)']");
+                
+                if(porcentajeActivo == null)
+                {
+                    porcentajeActivo = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($dataRed)']");
+                }
+
+                if (nombreActivo != null || cotizActivo != null || porcentajeActivo != null)
+                {
+                    this.nombre = nombreActivo.InnerText;
+                    this.cotizacion = cotizActivo.InnerText;
+                    this.porcentaje = porcentajeActivo.InnerText;
+                    this.codigo = codigo;
+                }
+                else
+                {
+                    throw new NullReferenceException("Ticker invalido.");
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ticker invalido.");
+            }
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Format("{0}", Nombre));
+            sb.AppendLine("====================================================");
+            sb.AppendLine(string.Format("${0} || {1}", this.cotizacion, Porcentaje));
+
+            return sb.ToString();
+        }
+
+        public static void TeclaEscape()
+        {
+            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+    }
+}
---
+++ TickerDesktopApp/TickerDesktopApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using Entidades;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TickerDesktopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+
+                //Console.Write("ESC para salir...");
+                Thread t = new Thread(Ticker.TeclaEscape);
+                t.Start();
+
+                try
+                {
+                    Console.Write("BUSCAR TICKER (ESC para salir)>> ");
+                    string search = Console.ReadLine().ToUpper();
+
+                    if(search != String.Empty)
+                    {
+                        Console.Clear();
+                        Ticker ticker = new Ticker(search);
+
+                        Console.WriteLine(ticker.MostrarDatos());
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No puede estar vacio.");
+                        Console.ReadKey();
+
+                    }
+                    Console.Clear();
+                }
+                catch (NullReferenceException e)
+                {
+                    Console.Clear();
+                    Console.WriteLine(e);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    Console.WriteLine(e);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
+        }
+    }
+}
